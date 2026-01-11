9e756f2b541c6551d008e637228d29463b9eb4ce
harshavardhanerigijja <harshavardhanerigijja@gmail.com>
2026-01-12 00:24:17 +0530

    Add files via upload

---
+++ rock-paper-scissors.py
@@ -0,0 +1,19 @@
+#rock,paper,scissors
+
+import random
+
+opt = ["Rock", "Paper", "Scissors"]
+a = input("choose Rock, Paper or Scissors:")
+b = random.choice(opt)
+
+print("You chose: ", a)
+print("Computer chose:", b)
+if a not in opt:
+    print("Invalid choice. Pick one from options")
+else:
+    if a == b:
+        print("Tie")
+    elif (a == "Rock" and b == "Paper") or (a == "Scissors" and b == "Rock") or (a == "Paper" and b == "Scissors"): 
+        print("You lose")
+    else:
+        print("You win")
